f99f6cdaa3db68124b08e4bb99830e2e70a0593e
Myles Grant <myles@mylesgrant.com>
2013-12-31 23:50:50 -0800

    Add an unauthed test

---
+++ client_test.go
@@ -0,0 +1,31 @@
+package coinbase
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestUnauthed(t *testing.T) {
+	c := &Client{}
+
+	body, err := c.Call("GET", "prices/spot_rate", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type Response struct {
+		Amount   string
+		Currency string
+	}
+
+	// parse into json
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Currency != "USD" {
+		t.Fatalf("Expected currency USD, got %s", response.Currency)
+	}
+}
